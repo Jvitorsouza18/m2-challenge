22c4bd131e396094b6d656cfc7aa68f842a336a7
João Vitor Souza Rocha <jvitorsouza18@gmail.com>
2022-08-04 15:45:51 -0300

    feat:

--- m2-challenge-front/src/components/PlansCard.vue
+++ m2-challenge-front/src/components/PlansCard.vue
@@ -1,8 +1,40 @@
 <template>
   <div>
-    <p>Cards here</p>
-    <div v-for="item in internetInfo" :key="item.id">
-      <div>{{ item.value }}</div>
+    <div>
+      <h1>Internet</h1>
+      <p>Selecione um plano de internet para continuar</p>
+      <div v-for="item in internetInfo" :key="item.id">
+        <button>
+          {{ `${item.value} MEGA` }}
+          <br />
+          {{ `R$ ${item.price}` }}
+          +DETALHES
+        </button>
+      </div>
+    </div>
+    <div>
+      <h1>Fixo</h1>
+      <p>Agora escolha seu pacote de internet fixo</p>
+      <div v-for="item in phoneInfo" :key="item.id">
+        <button>
+          {{ `${item.value.toUpperCase()} BRASIL` }}
+          <br />
+          {{ `R$ ${item.price}` }}
+          +DETALHES
+        </button>
+      </div>
+    </div>
+    <div>
+      <h1>TV</h1>
+      <p>Selecione um plano de tv para finalizar</p>
+      <div v-for="item in tvInfo" :key="item.id">
+        <button>
+          {{ `${item.value}` }}
+          <br />
+          {{ `R$ ${item.price}` }}
+          +DETALHES
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -13,17 +45,32 @@ export default {
   data() {
     return {
       internetInfo: [],
+      phoneInfo: [],
+      tvInfo: [],
     };
   },
+
   methods: {
     async getInternetInfo() {
       const plansInfo = await fetchAPI();
       this.internetInfo = plansInfo.internet;
-      console.log(this.internetInfo);
+    },
+
+    async getPhoneInfo() {
+      const plansInfo = await fetchAPI();
+      this.phoneInfo = plansInfo.phone;
+    },
+
+    async getTvInfo() {
+      const plansInfo = await fetchAPI();
+      this.tvInfo = plansInfo.tv;
     },
   },
+
   created() {
     this.getInternetInfo();
+    this.getPhoneInfo();
+    this.getTvInfo();
   },
 };
 </script>
