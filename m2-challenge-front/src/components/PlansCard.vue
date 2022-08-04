c9f7c6043f728723650ac7e6d4cfdcaa62c72939
João Vitor Souza Rocha <jvitorsouza18@gmail.com>
2022-08-04 16:06:33 -0300

    feat: button disabling

--- m2-challenge-front/src/components/PlansCard.vue
+++ m2-challenge-front/src/components/PlansCard.vue
@@ -4,7 +4,7 @@
       <h1>Internet</h1>
       <p>Selecione um plano de internet para continuar</p>
       <div v-for="item in internetInfo" :key="item.id">
-        <button>
+        <button v-on:click="getInternetPrice(item.price)">
           {{ `${item.value} MEGA` }}
           <br />
           {{ `R$ ${item.price}` }}
@@ -16,7 +16,10 @@
       <h1>Fixo</h1>
       <p>Agora escolha seu pacote de internet fixo</p>
       <div v-for="item in phoneInfo" :key="item.id">
-        <button>
+        <button
+          v-on:click="getPhonePrice(item.price)"
+          :disabled="internetPrice === 0"
+        >
           {{ `${item.value.toUpperCase()} BRASIL` }}
           <br />
           {{ `R$ ${item.price}` }}
@@ -28,7 +31,10 @@
       <h1>TV</h1>
       <p>Selecione um plano de tv para finalizar</p>
       <div v-for="item in tvInfo" :key="item.id">
-        <button>
+        <button
+          v-on:click="getTvPrice(item.price)"
+          :disabled="internetPrice === 0"
+        >
           {{ `${item.value}` }}
           <br />
           {{ `R$ ${item.price}` }}
@@ -36,6 +42,7 @@
         </button>
       </div>
     </div>
+    <h1>Total: {{ totalPrice }}</h1>
   </div>
 </template>
 
@@ -47,6 +54,10 @@ export default {
       internetInfo: [],
       phoneInfo: [],
       tvInfo: [],
+      internetPrice: 0,
+      phonePrice: 0,
+      tvPrice: 0,
+      cart: [],
     };
   },
 
@@ -65,12 +76,33 @@ export default {
       const plansInfo = await fetchAPI();
       this.tvInfo = plansInfo.tv;
     },
+
+    getInternetPrice(price) {
+      this.internetPrice = price;
+      console.log(this.internetPrice);
+    },
+
+    getPhonePrice(price) {
+      this.phonePrice = price;
+      console.log(this.phonePrice);
+    },
+
+    getTvPrice(price) {
+      this.tvPrice = price;
+      console.log(this.tvPrice);
+    },
   },
 
   created() {
     this.getInternetInfo();
     this.getPhoneInfo();
     this.getTvInfo();
+  },
+
+  computed: {
+    totalPrice() {
+      return (this.internetPrice + this.phonePrice + this.tvPrice).toFixed(2);
+    },
   },
 };
 </script>
